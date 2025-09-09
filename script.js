144ccb80402743e14f04948eaeca9f14aa910cba
diabla-20008 <00001094252736SP@al.educacao.sp.gov.br>
2025-09-09 10:45:45 -0300

    ma

--- script.js
+++ script.js
@@ -29,4 +29,7 @@ alternaContraste.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste');
 })
 
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded')
+botaoDeAcessibilidade.setAttribute('aria-expanded',!botaoSelecionado);
+
 })
